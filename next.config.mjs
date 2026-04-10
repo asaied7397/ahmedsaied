ac2486cdeb381db8d0bff60001df19db9ffebae5
asaied7397 <asaied7397@gmail.com>
2026-04-10 23:23:12 +0200

    edit 2

--- next.config.mjs
+++ next.config.mjs
@@ -2,18 +2,20 @@ import { withSentryConfig } from "@sentry/nextjs";
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default withSentryConfig(
-  nextConfig,
-  {
-    silent: true,
-    org: "javascript-mastery",
-    project: "javascript-nextjs",
-  },
-  {
-    widenClientFileUpload: true,
-    transpileClientSDK: true,
-    hideSourceMaps: true,
-    disableLogger: true,
-    automaticVercelMonitors: true,
-  },
-);
+export default nextConfig;
+
+// export default withSentryConfig(
+//   nextConfig,
+//   {
+//     silent: true,
+//     org: "javascript-mastery",
+//     project: "javascript-nextjs",
+//   },
+//   {
+//     widenClientFileUpload: true,
+//     transpileClientSDK: true,
+//     hideSourceMaps: true,
+//     disableLogger: true,
+//     automaticVercelMonitors: true,
+//   },
+// );
